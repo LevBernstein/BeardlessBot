f213065cf763854e2f9206ed93934f4354ea4944
LevBernstein <lev2000@me.com>
2020-10-23 11:33:31 -0400

    edge case handlig in dice; Verison 7.5

--- Bot.py
+++ Bot.py
@@ -447,49 +447,59 @@ async def on_message(text):
         print(finalString)
         await text.channel.send(finalString)
 
-    if text.content.startswith('!d') and not text.content.startswith('!deal'):
+    if text.content.startswith('!d') and ((text.content.split('!d',1)[1])[0]).isnumeric(): #The isnumeric check ensures that you can't activate this command by typing !deal or !debase or anything else.
         report = "Invalid side number. Enter 4, 6, 8, 10, 12, 20, or 100, as well as modifiers. No spaces allowed. Ex: !d4+3"
         command = text.content.split('!d',1)[1]
         print(command[0])
+        print(command)
+        isTen = False #Because !d10 and !d100 share their first two characters after the split, I was getting errors whenever I ran !d10 without a modifier.
+        #This boolean takes care of those errors. The problem arises because both the conditions for rolling a d10 and 2/3 of the conditions for rolling a d100
+        #would be met whenever the bot tried to roll a d10; then, when checking if command[2]=="0", I would get an array index out of bounds error, as the
+        #length of the command is actually only 2, not 3. However, with the boolean isTen earlier in the line, now it will never check to see if command has that
+        #third slot.
         if "-" in command:
             modifier = -1
         else:
             modifier = 1
-        if command[0] == "4":
-            if len(command)==1:
-                report = random.randint(1,4)
-            elif (command[1]=="+" or command[1] == "-"):
-                report = random.randint(1,4) + modifier*int(command[2:])
-        if command[0] == "6":
-            if len(command)==1:
-                report = random.randint(1,6)
-            elif (command[1]=="+" or command[1] == "-"):
-                report = random.randint(1,6) + modifier*int(command[2:])
-        if command[0] == "8":
-            if len(command)==1:
-                report = random.randint(1,8)
-            elif (command[1]=="+" or command[1] == "-"):
-                report = random.randint(1,8) + modifier*int(command[2:])
-        if command[0] == "1" and command[1] == "0":
-            if len(command)==2:
-                report = random.randint(1,10)
-            elif (command[2]=="+" or command[2] == "-"):
-                report = random.randint(1,10) + modifier*int(command[3:])
-        if command[0] == "1" and command[1] == "2":
-            if len(command)==2:
-                report = random.randint(1,12)
-            elif (command[2]=="+" or command[2] == "-"):
-                report = random.randint(1,12) + modifier*int(command[3:])
-        if command[0] == "2" and command[1] == "0":
-            if len(command)==2:
-                report = random.randint(1,20)
-            elif (command[2]=="+" or command[2] == "-") :
-                report = random.randint(1,20) + modifier*int(command[3:])
-        if command[0] == "1" and command[1] == "0" and command[2] == "0":
-            if len(command)==3:
-                report = random.randint(1,100)
-            elif (command[3]=="+" or command[3] == "-"):
-                report = random.randint(1,100) + modifier*int(command[4:])
+        if text.content == '!d2' or text.content == '!d1':
+            report = "Invalid side number. Enter 4, 6, 8, 10, 12, 20, or 100, as well as modifiers. No spaces allowed. Ex: !d4+3"
+        else:
+            if command[0] == "4":
+                if len(command)==1:
+                    report = random.randint(1,4)
+                elif (command[1]=="+" or command[1] == "-"):
+                    report = random.randint(1,4) + modifier*int(command[2:])
+            if command[0] == "6":
+                if len(command)==1:
+                    report = random.randint(1,6)
+                elif (command[1]=="+" or command[1] == "-"):
+                    report = random.randint(1,6) + modifier*int(command[2:])
+            if command[0] == "8":
+                if len(command)==1:
+                    report = random.randint(1,8)
+                elif (command[1]=="+" or command[1] == "-"):
+                    report = random.randint(1,8) + modifier*int(command[2:])
+            if command[0] == "1" and command[1] == "0":
+                isTen = True
+                if len(command)==2:
+                    report = random.randint(1,10)
+                elif (command[2]=="+" or command[2] == "-"):
+                    report = random.randint(1,10) + modifier*int(command[3:])
+            if command[0] == "1" and command[1] == "2":
+                if len(command)==2:
+                    report = random.randint(1,12)
+                elif (command[2]=="+" or command[2] == "-"):
+                    report = random.randint(1,12) + modifier*int(command[3:])
+            if command[0] == "2" and command[1] == "0":
+                if len(command)==2:
+                    report = random.randint(1,20)
+                elif (command[2]=="+" or command[2] == "-") :
+                    report = random.randint(1,20) + modifier*int(command[3:])
+            if isTen == False and command[0] == "1" and command[1] == "0" and command[2] == "0":
+                if len(command)==3:
+                    report = random.randint(1,100)
+                elif (command[3]=="+" or command[3] == "-"):
+                    report = random.randint(1,100) + modifier*int(command[4:])
         await text.channel.send(report)
 
     if text.content.startswith('!reset'):
