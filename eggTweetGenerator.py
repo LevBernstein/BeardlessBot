c9d175fa0e90c9820ea5154c87bd155f68b04d90
LevBernstein <lev2000@me.com>
2021-07-24 16:55:46 -0400

    Removes unused methods

--- eggTweetGenerator.py
+++ eggTweetGenerator.py
@@ -1,61 +1,32 @@
-#The following markov chain code was provided by CSTUY SHIP.
+#The following Markov chain code was provided by CSTUY SHIP.
 from random import choice, randint
 
 KEY_SIZE = 2
 FILE = 'resources/eggtweets_clean.txt'
 
-def read_file( fname ):
-    f = open( fname, 'r' )
-    text = f.read()
-    f.close()
-    return text
-
-def get_csv_list( fname ):
-    text = read_file( fname )
-    lines = text.split('\n')
-    line_list = []
-    for line in lines:
-        line_list.append( line.split(',') )
-    return line_list
-
-def get_csv_dict( fname ):
-    text = read_file( fname )
-    lines = text.split('\n')
-    line_dict = {}
-    for line in lines:
-        line_list = line.split(',')
-        key = line_list[0]
-        value = line_list[1:]
-        line_dict[ key ] = value
-    return line_dict
-
-def generate_chains( fname, key_size ):
+def generate_chains(fname, key_size):
+    with open(fname, 'r') as f:
+        text = f.read()
     chains = {}
-    text = read_file( fname )
     words = text.split()
-    i = 0
-    while i < len(words) - key_size:
-        key = ' '.join( words[i : i+key_size] ) 
+    for i in range(len(words) - key_size):
+        key = ' '.join(words[i : i + key_size]) 
         value = words[i + key_size]
         if key in chains:
-            chains[ key ].append( value )
+            chains[key].append(value)
         else:
             new_list = []
-            new_list.append( value )
-            chains[ key ] = new_list
-        i+= 1
+            new_list.append(value)
+            chains[key] = new_list
     return chains
 
-def generate_text( chains, num_words, key_size ):
-    key = choice( list(chains.keys()) )
+def generate_text(chains, num_words, key_size):
+    key = choice(list(chains.keys()))
     s = key
-    i = 0
-    while i < num_words:
-        word = choice( chains[ key ] )
-        s+= ' ' + word
-        key = ' '.join(key.split()[1 : key_size + 1])
-        key+= (' ' + word) if key_size > 1 else word
-        i+= 1
+    for i in range(num_words):
+        word = choice(chains[key])
+        s += ' ' + word
+        key = (' '.join(key.split()[1 : key_size + 1])) + (' ' + word) if key_size > 1 else word
     sourceTextCapital = s
     firstLetter = sourceTextCapital[0]
     sourceTextCapital = sourceTextCapital[1:]
@@ -64,9 +35,8 @@ def generate_text( chains, num_words, key_size ):
     return s
 
 def final():
-    randInt = randint(1,2)
-    chains = generate_chains( FILE, randInt )
-    text = generate_text( chains, randint(10,35), randInt )
+    complexity = randint(1, 2)
+    text = generate_text(generate_chains(FILE, complexity), randint(10, 35), complexity)
     print(text)
     return text
 
