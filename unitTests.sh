53907bffe9190a4e38a2e3b233f07d5c72980a10
Lev Bernstein <lev2000@me.com>
2024-05-11 16:49:55 -0400

    Truncate to .00 to fill the space

--- unitTests.sh
+++ unitTests.sh
@@ -7,7 +7,7 @@ echo "# Beardless Bot Unit Test Results" > testResults.md;
 python3 -m coverage run --include=$(echo $PWD)/* --omit=*/lib/* -m \
 	pytest -Wignore --ignore=*/lib/* --tb=line |& tee -a testResults.md;
 python3 -m coverage report -m |& tee -a testResults.md;
-python3 -m coverage xml;
+python3 -m coverage xml 1>/dev/null;
 rm resources/images/coverage.svg 2>/dev/null;
 genbadge coverage -i coverage.xml -o resources/images/coverage.svg
 rm .coverage 2>/dev/null;
@@ -17,4 +17,4 @@ rm coverage.xml 2>/dev/null;
 python3 -c '
 import re
 with open("resources/images/coverage.svg", "r") as f: svg = f.read()
-with open("resources/images/coverage.svg", "w") as g: g.write(re.sub(r"((\.\d{2})%+)", "%", svg))'
+with open("resources/images/coverage.svg", "w") as g: g.write(re.sub(r"((\.\d{2})%+)", ".00%", svg))'
